7e16ad95519cb8c00a3c21bec78cdbccac58361c
Uaking <blackkonga@gmail.com>
2022-10-13 01:49:18 -0700

    :

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
 
-/* To print a sentence */
+/**
+ * main - prints the phrase "Programming is like building amultilingual puzzle
+ * Return: 0 if exited properly, not zero if otherwise
+ */
 int main(void)
 {
-	puts ("\"Programming is like building a multilingual puzzle")
-	return(0)
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
