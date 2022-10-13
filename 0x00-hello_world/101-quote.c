1fb0430238d197269290be756fe492a8d2a438ab
Uaking <blackkonga@gmail.com>
2022-10-13 11:11:33 +0100

    :

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main - prints a sentence
+ * Return: Always 0 (sucessful)
+ */
+
+int main(void)
+{
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
+}
