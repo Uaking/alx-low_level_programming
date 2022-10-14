1376d24ed1ae371db8f45a9a41fbfeb7a2fd73f2
Uaking <blackkonga@gmail.com>
2022-10-14 01:38:33 -0700

    :

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,18 +1,23 @@
 #include <stdio.h>
 
 /**
- * main - prints a character
+ * main - prints a program
  * Return: 0 if sucessful
  */
 
 int main(void)
 {
+
 	char ch;
 
-        for (ch = 'a'; ch <= 'z'; ch++)
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
 		putchar(ch);
-	for (ch = 'A': ch <= 'z'; ch++)
+	}
+	for (ch = 'A'; ch <= 'Z'; ch++)
+	{
 		putchar(ch);
-        putchar('\n');
-        return (0);
+	}
+	putchar('\n');
+	return (0);
 }
