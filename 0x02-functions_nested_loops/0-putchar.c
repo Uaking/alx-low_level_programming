617a169954bdd4cd05177a6ecea434b64ca49844
Uaking <blackkonga@gmail.com>
2022-10-18 07:29:54 +0100

    *

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <main.h>
+
+/**
+ * main - starts a program
+ * Return: 0 if sucessful
+ */
+
+int main(void)
+{
+	 _putchar('H');
+	 _putchar('o');
+	 _putchar('l');
+	 _putchar('b');
+	 _putchar('e');
+	 _putchar('r');
+	 _putchar('t');
+	 _putchar('o');
+	 _putchar('n');
+	 _putchar('\n');
+
+	return (0);
+}
+	
