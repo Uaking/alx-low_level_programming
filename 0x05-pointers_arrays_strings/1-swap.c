ee13d03841c13e6b9dc1d72101922d1ae510d5e0
blackkonga@gmail.com <blackkonga@gmail.com>
2022-10-24 09:07:44 +0100

    ok

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -6,12 +6,12 @@
  * @a: - first integer
  * @b: - second integer
  */
- 
+
 void swap_int(int *a, int *b)
 {
-  int c;
-  
-  c = *a;
-  *a = *b;
-  *b = c;
+int c;
+
+c = *a;
+*a = *b;
+*b = c;
 }
