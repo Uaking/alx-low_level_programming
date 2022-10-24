091fab67ea26328b018fe8af861d83ade59c8da4
blackkonga@gmail.com <blackkonga@gmail.com>
2022-10-24 09:18:29 +0100

    ok

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strlen - returns size of a char
+ * @s: the char to check
+ */
+
+int _strlen(char *s)
+{
+int i;
+i = 0;
+
+while (s[i] != '\0')
+{
+i++;
+}
+return (i)
+}
