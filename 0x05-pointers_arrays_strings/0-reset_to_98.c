62a62da231cc52fb53bc7f4fcaff6987cd5181dc
blackkonga@gmail.com <blackkonga@gmail.com>
2022-10-24 08:58:08 +0100

    ok

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * reset_to_98 - resets a pointer to 98
+ * @n - integer to be checked
+ * Return void
+ */
+ 
+ void reset_to_98(int *n)
+ {
+   *n = 98;
+ }
---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * swap_int - swaps two pointers
+ * @a - first integer
+ * @b - second integer
+ */
+ 
+ void swap_int(int *a, int *b)
+ {
+   int c;
+   
+   c = *a;
+   *a = *b;
+   *b = c;
+ }
