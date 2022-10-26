d2355dedbba8a433de67e81119ab4568056f4c96
blackkonga@gmail.com <blackkonga@gmail.com>
2022-10-26 20:12:06 +0100

    n

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,16 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "main.h"
 
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
 	int i;
 	i = 0;
-	int n11 = atoi (n1);
-	int n22 = atoi (n2);
-	int a = n11 + n22;
-	
-	sprintf(r, "%d", a);
+	unsigned long long int n11 = strtoull (n1, NULL, 10);
+	unsigned long long int n22 = strtoull (n2, NULL, 10);
+	unsigned long long int a = n11 + n22;
+		
+	sprintf(r, "%llu", a);
 	
 	while(r[i] != '\0')
 	{
