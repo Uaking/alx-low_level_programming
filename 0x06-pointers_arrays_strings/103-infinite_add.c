ac61449f0afc771d1a9940f743fd7a5cbe3540e8
blackkonga@gmail.com <blackkonga@gmail.com>
2022-10-26 14:29:12 +0100

    n

---
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+char *infinite_add(char *n1, char *n2, char *r, int size_r)
+{
+	int i;
+	i = 0;
+	int n11 = atoi (n1);
+	int n22 = atoi (n2);
+	int a = n11 + n22;
+	r[size_r] = 0;
+	
+	
+	sprintf(r, "%d", a);
+	
+	while(r[i] != '\0')
+	{
+		i++;
+	}
+	
+	if ( i >= size_r)
+	{
+		return (0);
+	}
+	else if (i < size_r)
+	{
+		return (r);
+	}
+}
