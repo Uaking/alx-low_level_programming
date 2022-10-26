c9974b6e22bd989d1a9d116155d8330c6cf79589
blackkonga@gmail.com <blackkonga@gmail.com>
2022-10-26 10:21:27 +0100

    first

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _strcat - merges two strings
+ * @dest: first integer
+ * @src: source file
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int i, z;
+	i = 0;
+	
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+	
+	for (z = 0; src[z] != '\0'; z++)
+	{
+		i++;
+		dest[i] = src[z];
+	}
+	return (dest);
+}
