7c4fb34981037cc9aeaf896346cd51a96bfe1ccd
Uaking <blackkonga@gmail.com>
2022-11-08 11:23:40 +0100

    :

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -14,22 +14,21 @@ char *create_array(unsigned int size, char c)
 	char *ptr;
 	unsigned int i;
 
-	ptr = malloc(size * sizeof(c));
-
 	if (size == 0)
 	{
 		return (NULL);
 	}
+	ptr = malloc(size * sizeof(c));
 
 	if (ptr = NULL)
-	{
 		return (NULL);
-	}
 
 	else
 	{
 		for (i = 0; i < size; i++)
+		{
 			ptr[i] = c;
 			return (ptr);
+		}
 	}
 }
