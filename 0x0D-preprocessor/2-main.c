85ff4974ce90d3ba016593b32397635b56d64a76
blackkonga@gmail.com <blackkonga@gmail.com>
2022-11-14 12:31:34 +0100

    preprocessor ediit

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,15 +1,12 @@
 #include <stdio.h>
 
 /**
- * main - our main file
- * Return: 0 if sucessful
+ * main - prints the name of the file
+ *
+ * Return: Always 0 (Success)
  */
-
-
 int main(void)
 {
-  char x = __FILE__;
-
-  printf("%s\n",x);
-  return (0);
+	printf("%s\n", __FILE__);
+	return (0);
 }
