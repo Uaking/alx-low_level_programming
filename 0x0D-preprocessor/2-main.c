476f133ced41f26320497154ee9fcb6391edf4f7
blackkonga@gmail.com <blackkonga@gmail.com>
2022-11-14 11:44:53 +0100

    preprocessor

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - our main file
+ * Return: 0 if sucessful
+ */
+
+
+int main(void)
+{
+  char x = __FILE__;
+
+  printf("%s\n",x);
+  return (0);
+}
---
+++ 0x0D-preprocessor/README.md
@@ -0,0 +1,1 @@
+my gooooooooooood readme file
